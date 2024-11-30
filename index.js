4cec69a25795e5480483e5eb39052d8db10bcb53
Kristian Aarol T. Valdez <qkavaldez01@tip.edu.ph>
2024-11-30 07:59:27 +0000

    Updated hmtl, css,jscript || added feature modal || cart page desgin

--- index.js
+++ index.js
@@ -1,67 +1,104 @@
 var mainData = [];
+
+// Display phones function
 const displayPhones = async () => {
-    try{
-        const respone = await fetch('data.json');
-        mainData = await respone.json();
-        const {products} = mainData;
-        console.log(products);
+    try {
+        const response = await fetch('data.json'); 
+        mainData = await response.json();
+        const { products } = mainData;
 
-       //iphone display
-        const iphone = products.filter((element) => element.category == "iphone");
-        console.log(iphone);
-         document.querySelector(".iphone").innerHTML = iphone.map((element, index) => {
-            const {image, name, price} = element;
-            return`
+        // iPhone display
+        const iphone = products.filter((element) => element.category === "iphone");
+        document.querySelector(".iphone").innerHTML = iphone.map((element, index) => {
+            const { image, name, price } = element;
+            return `
             <div class="items">
-              <img src="${image}" alt="${name}" />
+              <img src="${image}" onclick="showModal(${index}, 'iphone')" " alt="${name}"/>
               <h4>${name}</h4>
-            <button onclick="showCart(${index}, 'iphone')"><ion-icon name="add-circle-outline" class="addToCartBtn"></ion-icon></button>
+                   <button onclick="showCart(${index}, 'iphone')"><ion-icon name="add-circle-outline" class="addToCartBtn"></ion-icon></button>
+              </button>
             </div>
-            `
-         }).join('');
-       
-        //case display
-        const phoneCase = products.filter((element) => element.category == "phonecase");
-       document.querySelector(".phone-case").innerHTML = phoneCase.map((element, index) => { 
-           const {image, name, price} = element;
-              return`
-                <div class="items">
-                <img src="${image}" alt="${name}" />
-                  <h4>${name}</h4>
-                  <button onclick="showCart(${index}, 'phonecase')"><ion-icon name="add-circle-outline" class="addToCartBtn"></ion-icon></button>
-                 </div>
-              `   
-}).join('');
+            `;
+        }).join('');
 
-const android = products.filter((element) => element.category == "android");
-console.log(iphone);
- document.querySelector(".android").innerHTML = android.map((element, index) => {
-    const {image, name, price} = element;
-    return`
-    <div class="items">
-      <img src="${image}" alt="${name}" />
-      <h4>${name}</h4>
-    <button onclick="showCart(${index}, 'android')"><ion-icon name="add-circle-outline" class="addToCartBtn"></ion-icon></button>
-    </div>
-    `
-}).join('');
+        // Phone case display
+        const phoneCase = products.filter((element) => element.category === "phonecase");
+        document.querySelector(".phone-case").innerHTML = phoneCase.map((element, index) => {
+            const { image, name, price } = element;
+            return `
+            <div class="items">
+              <img src="${image}" onclick="showModal(${index}, 'phonecase')" " />
+              <h4>${name}</h4>
+                   <button onclick="showCart(${index}, 'phonecase')"><ion-icon name="add-circle-outline" class="addToCartBtn"></ion-icon></button>
+              </button>
+            </div>
+            `;
+        }).join('');
 
-        //EARBUDS DISPLAY
-        const earbuds = products.filter((element) => element.category == "earbuds");
-        document.querySelector(".Earbuds").innerHTML = earbuds.map((element, index) => { 
-            const {image, name, price} = element;
-               return`
-                 <div class="items">
-                 <img src="${image}" alt="${name}" />
-                   <h4>${name}</h4>
-                   <button onclick="showCart(${index}, 'earbuds')"><ion-icon name="add-circle-outline" class="addToCartBtn"></ion-icon></button>
-                  </div>
-               `   
- }).join('');
-        //HEADPHONES DISPLAY
+        // Android display
+        const android = products.filter((element) => element.category === "android");
+        document.querySelector(".android").innerHTML = android.map((element, index) => {
+            const { image, name, price } = element;
+            return `
+            <div class="items" ">
+              <img src="${image}" aonclick="showModal(${index}, 'android')" " />
+              <h4>${name}</h4>
+                   <button onclick="showCart(${index}, 'android')"><ion-icon name="add-circle-outline" class="addToCartBtn"></ion-icon></button>
+              </button>
+            </div>
+            `;
+        }).join('');
+
+        // Earbuds display
+        const earbuds = products.filter((element) => element.category === "earbuds");
+        document.querySelector(".Earbuds").innerHTML = earbuds.map((element, index) => {
+            const { image, name, price } = element;
+            return `
+            <div class="items">
+              <img src="${image}" onclick="showModal(${index}, 'earbuds')" " />
+              <h4>${name}</h4>
+                <button onclick="showCart(${index}, 'earbuds')"><ion-icon name="add-circle-outline" class="addToCartBtn"></ion-icon></button>
+              </button>
+            </div>
+            `;
+        }).join('');
+
     }catch(error){
         console.log(error);
     }
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayPhones();
+
+    const closeModalButton = document.getElementById('close-modal-btn');
+    closeModalButton.addEventListener('click', closeModal);
+});
+
+function showModal(index, category) {
+    const products = mainData.products.filter((product) => product.category === category);
+    const product = products[index];
+
+    document.getElementById("modal-image").src = product.image;
+    document.getElementById("modal-name").innerText = product.name;
+    document.getElementById("modal-price").innerText = product.price;
+    document.getElementById("modal-storage").innerText = product.storage || "N/A";
+    document.getElementById("modal-colors").innerText = product.colors?.join(", ") || "N/A";
+
+    const modal = document.getElementById("product-modal");
+    modal.style.display = "flex";
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    };
+}
+
+function closeModal() {
+    const modal = document.getElementById("product-modal");
+    modal.style.display = "none";
 }
 
 document.addEventListener('DOMContentLoaded', displayPhones);
